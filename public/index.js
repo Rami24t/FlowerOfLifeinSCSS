b5c48b391cead3adce96e18da2a6ce6f0ffeefe2
Rami <alsaadi.rami24@gmail.com>
2023-12-07 02:01:33 +0100

    format js code

--- public/index.js
+++ public/index.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const container = document.querySelector(".container");
-  
-    for (let h = 0; h < 12; h++)
-    {
-      const majorGroup = document.createElement("div");
-      for ( let i = 0; i < 12; i++)
-      {
-        const minorGroup = document.createElement("div");
-        // for (let j = 0; j < 12; j++)
-        // {
-        //   const circle = document.createElement("div");
-        //   minorGroup.appendChild(circle);
-        // }
-        majorGroup.appendChild(minorGroup);
-      }
-    container.appendChild(majorGroup);
+  const container = document.querySelector(".container");
+
+  for (let h = 0; h < 12; h++) {
+    const majorGroup = document.createElement("div");
+    for (let i = 0; i < 12; i++) {
+      const minorGroup = document.createElement("div");
+      // for (let j = 0; j < 12; j++) {
+      //   const circle = document.createElement("div");
+      //   minorGroup.appendChild(circle);
+      // }
+      majorGroup.appendChild(minorGroup);
     }
-  });
-  
+    container.appendChild(majorGroup);
+  }
+});
